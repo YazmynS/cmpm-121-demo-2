b0869ad80fb7613f6e9dbbf63d899ae2658137c1
YazmynS <122718475+YazmynS@users.noreply.github.com>
2024-10-16 14:44:13 -0700

    Step 2: Draw/Clear (Complete)

--- src/main.ts
+++ src/main.ts
@@ -16,6 +16,32 @@ canvasElement.id = "gameCanvas";
 canvasElement.width = 256;
 canvasElement.height = 256;
 
-// Append both elements
+//Create clear button 
+const clearButton = document.createElement("button");
+clearButton.textContent = "Clear";
+clearButton.id = "clearButton";
+
+// Append elements
 app.appendChild(titleElement);
 app.appendChild(canvasElement);
+app.appendChild(clearButton);
+
+const render = canvasElement.getContext("2d")!;
+let drawing = false;
+
+canvasElement.addEventListener("mousedown", (event) => {
+  drawing = true;
+  render.beginPath();
+  render.moveTo(event.offsetX, event.offsetY);
+});
+
+canvasElement.addEventListener("mousemove", (event) => {
+  if (drawing) {
+    render.lineTo(event.offsetX, event.offsetY);
+    render.stroke();
+  }
+});
+
+clearButton.addEventListener("click", () => {
+  render.clearRect(0, 0, canvasElement.width, canvasElement.height);
+});
